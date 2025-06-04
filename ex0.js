8ecb0e9424de0dfdd70b214075c8b2e28b87406f
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-06-03 22:19:01 -0300

    ex0.js

--- ex0.js
+++ ex0.js
@@ -1,18 +1,20 @@
-// Defina os três lados do triângulo
+// os lados do triângulo
 let a = 3;
 let b = 4;
 let c = 5;
 
-// Verifica se os lados formam um triângulo válido
+// se o triângulo é válido
 if (a + b > c && a + c > b && b + c > a) {
-  // Classifica o tipo de triângulo
-  if (a === b && b === c) {
+  //  tipo de triângulo
+ } if (a === b && b === c) {
     console.log("Equilátero");
   } else if (a === b || a === c || b === c) {
     console.log("Isósceles");
   } else {
     console.log("Escaleno");
-  }
-} else {
-  console.log("Não é um triângulo válido");
 }
+
+
+// Equilátero : Todos os lados iguais
+// Isóceles : Dois lados iguais
+// Escaleno : Todos os lados iguais
